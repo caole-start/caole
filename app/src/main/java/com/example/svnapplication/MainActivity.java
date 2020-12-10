5354f3ad0f1c941162576278bcf7bd91c0fc18c4
caole <1060736854@qq.com>
2020-12-10 16:31:42 +0800

    啊实打实

---
+++ app/src/main/java/com/example/svnapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.svnapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    String a = "" ;
+  String s = "aaaaa" ;
+  String l = "宋俊颍" ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
